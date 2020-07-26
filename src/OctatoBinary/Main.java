53d51893a4723edd089a606ecddb22b5193a0586
daesoo <daesoo@192.168.0.12>
2020-07-26 14:29:42 +0900

    Octa num to Binary(solved)

--- src/OctatoBinary/Main.java
+++ src/OctatoBinary/Main.java
@@ -3,62 +3,31 @@ package OctatoBinary;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int x = Integer.parseInt(br.readLine());
-		if(x==0) {
-			System.out.println("000");
-			return ;
+		String[] str = {"000","001","010","011","100","101","110","111"};
+		String s = br.readLine();
+		int[] arr = new int[s.length()];
+		StringBuilder sb = new StringBuilder();
+		for(int i =0; i<arr.length; i++) {
+			arr[i] = s.charAt(i)-48;
+			sb.append(str[arr[i]]);
 		}
-		Queue<Integer> q = new LinkedList<Integer>();
-		int temp = 0;
-		while (x != 0) {
-			temp = x % 10;
-			q.offer(temp);
-			x /= 10;
+		if(sb.charAt(0)=='0'&& sb.charAt(1)=='0') {
+			sb.delete(0, 2);
+			System.out.println(sb);
 		}
-		int[] arr = new int[q.size()];
-		int[] nums = new int[q.size()];
-		arr[0]=1;
-		for(int i=1; i<arr.length; i++) {
-			arr[i] = (int)(Math.pow(8, i));
+		else if(sb.charAt(0)=='0') {
+			sb.deleteCharAt(0);
+			System.out.println(sb);
 		}
-		for(int i=0; i< nums.length; i++) {
-			nums[i] = q.poll();
+		else {
+			System.out.println(sb);
 		}
-		int total=0;
-		for(int i=0; i<nums.length;i++) {
-			total+= arr[i]*nums[i];
-		}
-		StringBuilder sb =  new StringBuilder();
-		Stack<Integer> stack = new Stack<Integer>();
-		
-		while(total!=1) {
-			if(total%2==0) {
-				stack.push(0);
-				total=total/2;
-				if(total==1) {
-					stack.push(1);
-				}
-			}else {
-				stack.push(1);
-				total=total/2;
-				if(total==1) {
-					stack.push(1);
-				}
-			}
-		}
-			
-		while(!stack.isEmpty()) {
-			sb.append(stack.pop());
-		}
-		
-		System.out.println(sb);
 	}
 }
