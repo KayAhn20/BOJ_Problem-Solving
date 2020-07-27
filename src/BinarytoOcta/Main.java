2c1c04d1ee075c98037637e16bea5254613dc07d
daesoo <daesoo@192.168.0.12>
2020-07-27 21:34:46 +0900

    still on ma way

--- src/BinarytoOcta/Main.java
+++ src/BinarytoOcta/Main.java
@@ -8,32 +8,56 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int x = Integer.parseInt(br.readLine());
-		int[] arr = new int[1000000];
-		for(int i =0; i<x; i++) {
-			arr[i]=x%10;
-			x=x/10;
-			System.out.print(arr[i]);
+		StringBuilder sb = new StringBuilder();
+		StringBuilder answer = new StringBuilder();
+		
+		String str = br.readLine();
+		int zero_test = Integer.parseInt(str);
+		if(zero_test==0) {
+			answer.append(0);
+		}else {
+			if(str.length()%2==0) {
+				sb.append("0");
+				sb.append(str);
+			}else {
+				sb.append(str);
+			}
+			String ready = sb.toString();
+			int[] arr = new int[ready.length()/3];
+			for(int i=0; i<arr.length; i++) {
+				arr[i] = Integer.parseInt(ready.substring(i*3, (i*3)+3));
+			}
+			for(int i=0; i<arr.length; i++) {
+				if(arr[i]==0) {
+					arr[i]=0;
+					
+				}else if(arr[i]==1) {
+					arr[i]=1;
+					answer.append(arr[i]);
+				}else if(arr[i]==10) {
+					arr[i]=2;
+					answer.append(arr[i]);
+				}else if(arr[i]==11) {
+					arr[i]=3;
+					answer.append(arr[i]);
+				}else if(arr[i]==100) {
+					arr[i]=4;
+					answer.append(arr[i]);
+				}else if(arr[i]==101) {
+					arr[i]=5;
+					answer.append(arr[i]);
+				}else if(arr[i]==110) {
+					arr[i]=6;
+					answer.append(arr[i]);
+				}else if(arr[i]==111) {
+					arr[i]=7;
+					answer.append(arr[i]);
+				}
+			}
 		}
-		System.out.println();
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		bw.write(answer+"");
+		bw.flush();
+		bw.close();
+
 	}
 }
