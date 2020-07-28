5024a61cb64a4d4568fad29527c02a085b739f5c
daesoo <daesoo@192.168.0.12>
2020-07-28 11:55:28 +0900

    solved

--- src/가장많은글자/Main.java
+++ src/가장많은글자/Main.java
@@ -1,14 +1,32 @@
 package 가장많은글자;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+/*
+ * 
+ *  1371번 문제  
+ * 
+ */
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int[] alpha = new int[26];
-		
-	}
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int alpha[] = new int[26];
+        int max = 0;
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            for(int i=0; i<str.length(); i++) {
+            	if(str.charAt(i)!=' ') {
+            		alpha[str.charAt(i)-97]++;
+            		if(alpha[str.charAt(i)-97]>max) {
+            			max = alpha[str.charAt(i)-97];
+            		}
+            	}
+            }
+        }
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] == max) {
+            	System.out.print((char)(i + 97));
+            }
+        }
+        sc.close();
+    }
 }
